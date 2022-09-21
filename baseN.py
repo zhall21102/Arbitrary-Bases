9a77dce529e738615a2b38e2ae8d911c41305056
zhall21102 <109304863+zhall21102@users.noreply.github.com>
2022-09-21 16:39:05 -0400

    Documentation Commit

--- baseN.py
+++ baseN.py
@@ -1,13 +1,14 @@
 import math
 possible='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 class BaseN:
-    def __init__(self, value, base, chrSet):
+    """A class to handle arithmetic between bases"""
+    def __init__(self, value, base, chrSet = possible):
         self.base = base
         self.chrSet = chrSet
         if type(value)== str:
             value=value.upper()
             self.bd=value
-            self.decimal = BaseNToDecimal(value, self.base, self.chrSet)
+            self.decimal = baseNToDecimal(value, self.base, self.chrSet)
         else:
             self.bd=decimalToBaseN(value, self.base, self.chrSet)
             self.decimal=value
@@ -61,14 +62,15 @@ class BaseN:
         return self.decimal <= other.decimal
         
     def __repr__(self):
-        return 'Biker\'s Dozenal number {}.\nDecimal interpretation: {}.'\
-               .format(self.bd,self.decimal)
+        return 'Base {} number {}.\nDecimal interpretation: {}.'\
+               .format(self.base, self.bd, self.decimal)
     def __str__(self):
         return self.bd
 
 
     
-def decimalToBaseN(n, base, chrSet):
+def decimalToBaseN(n, base, chrSet = possible):
+    """Converts a number N into its equivalent in base BASE"""
     if n<0:
         negative = True
         n=abs(n)
@@ -134,11 +136,12 @@ def decimalToBaseN(n, base, chrSet):
         else:
             return integer+'.'+decimal
 
-def helper(n, base, chrSet):
+def helper(n, base, chrSet): #Helper for decimalToBaseN()
     return n//base,n%base
 
 
-def BaseNToDecimal(n, base, chrSet):
+def baseNToDecimal(n, base, chrSet = possible):
+    """Convert the value of N in base BASE into its decimal equivalent"""
     if '-' in n:
         negative = True
         n=n.replace('-','')
@@ -154,7 +157,7 @@ def BaseNToDecimal(n, base, chrSet):
         else:
             return output
     else:
-        integer=BaseNToDecimal(n[:n.index('.')], base, chrSet)
+        integer=baseNToDecimal(n[:n.index('.')], base, chrSet)
         decimal=0
         n = n[n.index('.')+1:]
         for place in range(len(n)):
@@ -166,11 +169,13 @@ def BaseNToDecimal(n, base, chrSet):
 
 
 def fractions(base, chrSet = possible):
+    """Print all fractions in the form 1/x up to x = BASE"""
     for e in range(base):
         print(BaseN(1, base, chrSet)/BaseN((e+1), base, chrSet),\
               '1/{}'.format(e+1))
         
 def fractionsTo(start, limit):
+    """Execute fractions() for every base from START to LIMIT"""
     for e in range(start, limit+1):
         print('base',e)
         fractions(e, possible[:e])
@@ -178,6 +183,7 @@ def fractionsTo(start, limit):
 
 
 def compareBases(largest, number):
+    """Convert a NUMBER in every base up to LARGEST"""
     print(number, 'in every base up to base', largest)
     print('Base 1: ',end = '')
     if number%1 == 0:
@@ -189,23 +195,23 @@ def compareBases(largest, number):
     for e in range(largest-1):
         base = e+2
         chrSet=possible[:base]
-        #print(chrSet)
-        #print(BaseNToDecimal('10'))
         print('Base {}: {}'.format(base,decimalToBaseN(number, base, chrSet)))
 
 def convertBases(value, base1, base2 = 10):
+    """Convert a VALUE in BASE1 to the equivalant in BASE2"""
     value = str(value)
     print(value, 'in base', base1, 'is equal to', \
-            decimalToBaseN(BaseNToDecimal(value, base1, possible[:base1]),base2,possible[:base2]),\
+            decimalToBaseN(baseNToDecimal(value, base1, possible[:base1]),base2,possible[:base2]),\
             'in base',base2)
-    return decimalToBaseN(BaseNToDecimal(value, base1, possible[:base1]),base2,possible[:base2])
+    return decimalToBaseN(baseNToDecimal(value, base1, possible[:base1]),base2,possible[:base2])
 
 
-def findInt(base, number):
+def findInt(base, number): #Helper for findPi()
     chrSet=possible[:math.ceil(base)]
     return decimalToBaseN(number, base, chrSet)
     
 def findPi(base,accuracy = 'full'):
+    """Approximate Pi in a given BASE"""
     chrSet=possible[:math.ceil(base)]
     pi = math.pi
     approx = str(findInt(base, 3)) + '.'
@@ -214,22 +220,17 @@ def findPi(base,accuracy = 'full'):
             approx+='0'
             for e in range(math.ceil(base)):
                 approx = list(approx)
-                #print(chrSet[-e-1])
                 approx[-1] = chrSet[-e-1]
-                #print(approx)
-                step = BaseNToDecimal(approx,base,chrSet)
-                #print(step)
+                step = baseNToDecimal(approx,base,chrSet)
                 if step <= math.pi:
                     break
     else:
-        while BaseNToDecimal(approx,base,chrSet) != math.pi:
+        while baseNToDecimal(approx,base,chrSet) != math.pi:
             approx+='0'
             for e in range(math.ceil(base)):
                 approx = list(approx)
                 approx[-1] = chrSet[-e-1]
-                #print(approx)
-                step = BaseNToDecimal(approx,base,chrSet)
-                #print(step)
+                step = baseNToDecimal(approx,base,chrSet)
                 if step <= math.pi:
                     break
             
@@ -239,49 +240,45 @@ def intPi(fro,to):
     for e in range(fro,to+1):    
         base = e
         chrSet=possible[:base]
-
         pi=''.join(findPi(base))
-        #print(BaseNToDecimal(pi,base,possible))
         print('{}: {}'.format(base, pi))
-        #print(math.pi)
-
 
 
         
-def findIntValueHelp(base, value):
+def findIntValueHelp(base, value): #Helper for findIntValue()
     chrSet=possible[:math.ceil(base)]
     length = 1
     approx = ['0']
-    while BaseNToDecimal(approx,base,chrSet) < value:
+    while baseNToDecimal(approx,base,chrSet) < value:
         approx = ['0'] * length
         for e in chrSet:
             approx[0] = e
-            if BaseNToDecimal(approx,base,chrSet) >= value:
+            if baseNToDecimal(approx,base,chrSet) >= value:
                 break
         approx.insert(0,'0')
         length += 1
     approx = approx[1:]
-    if BaseNToDecimal(approx,base,chrSet) > value and int(approx[0])>1:
+    if baseNToDecimal(approx,base,chrSet) > value and int(approx[0])>1:
         approx[0] = str(int(approx[0])-1)
-    if BaseNToDecimal(approx,base,chrSet) > value:
+    if baseNToDecimal(approx,base,chrSet) > value:
         newLength = len(approx)-1
         approx = ['0'] * newLength
         approx[0] = str(math.ceil(base)-1)
     return approx
 
-def findIntValue(base, value):
+def findIntValue(base, value): #Helper for findValue()
     chrSet=possible[:math.ceil(base)]
     initial = findIntValueHelp(base, value)
     if initial[0] == '0':
         initial[1] = str(math.ceil(base)-1)
         initial = initial[1:]
-    remainder = value - BaseNToDecimal(initial,base,chrSet)
+    remainder = value - baseNToDecimal(initial,base,chrSet)
     for e in initial:
         cur = initial
         if remainder != 0:
             test = list(initial)
             test[-1] = '1'
-            if BaseNToDecimal(test,base,chrSet) > value:
+            if baseNToDecimal(test,base,chrSet) > value:
                 return ''.join(initial)
             try:
                 nex = (findIntValueHelp(base, remainder))
@@ -290,18 +287,19 @@ def findIntValue(base, value):
             if len(str(cur)) != len(str(nex)):
                 test = cur
                 test[-1]=str(int(test[-1])+1)
-                if BaseNToDecimal(test,base,chrSet) > value:
+                if baseNToDecimal(test,base,chrSet) > value:
                     test[-1]=str(int(test[-1])-1)
                     return ''.join(test)
                 cur[-len(nex):]=nex
                 initial = cur
             try:
-                remainder = value - BaseNToDecimal(initial,base,chrSet)
+                remainder = value - baseNToDecimal(initial,base,chrSet)
             except:
                 return ''.join(initial)
     return ''.join(initial)
     
 def findValue(base, value, accuracy = 'full'):
+    '''Approximate VALUE in a given BASE'''
     chrSet=possible[:math.ceil(base)]
     if value >= 1:
         approx = findIntValue(base, value)+'.'
@@ -313,31 +311,24 @@ def findValue(base, value, accuracy = 'full'):
             for e in range(math.ceil(base)):
                 approx = list(approx)
                 approx[-1] = chrSet[-e-1]
-                step = BaseNToDecimal(approx,base,chrSet)
+                step = baseNToDecimal(approx,base,chrSet)
                 if step <= value:
                     break
     else:
-        while BaseNToDecimal(approx,base,chrSet) != value:
+        while baseNToDecimal(approx,base,chrSet) != value:
             approx+='0'
             for e in range(math.ceil(base)):
                 approx = list(approx)
                 approx[-1] = chrSet[-e-1]
-                step = BaseNToDecimal(approx,base,chrSet)
+                step = baseNToDecimal(approx,base,chrSet)
                 if step <= value:
                     break
             
     return ''.join(approx)
 
-n=7
-#print(BaseNToDecimal('120',2,possible))
-#print(BaseNToDecimal('4.009005009006001003004006007005',-10**(1/3),possible))
-#print(BaseNToDecimal('500007000060000400003000010000600009000050000900004',-1/10**(1/5),possible))
-print(decimalToBaseN(n,2,possible))
-print(decimalToBaseN(24*n,2,possible))
 
 
 
 #10 approximated in base pi
-#Call me Todd Howard, because it just works
-#print(BaseNToDecimal('23.2021120021000000300201212221',math.pi,possible))
-#print(BaseNToDecimal('100.01022122221121122001111210202',math.pi,possible))
+#print(baseNToDecimal('23.2021120021000000300201212221',math.pi,possible))
+#print(baseNToDecimal('100.01022122221121122001111210202',math.pi,possible))
